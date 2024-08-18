09bc54d092c6e8c647b4e45c2094b46ce9920185
Pranav <pranavlakkadi@gmail.com>
2024-08-18 10:59:55 +0530

    first commit

--- 8-Horse-Store/src/horseStore_V1/IhorseStore.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
-interface IhorseStore {
-    function updateNumberOfHorses(uint256 _numberOfHorses) external;
-
-    function getNumberOfHorses() external view returns (uint256);
-}
